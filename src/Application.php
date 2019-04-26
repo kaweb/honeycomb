1767433e7cded2bc62e645dac9d397c68a81ddbe
Alex Wilkinson <alex@kaweb.co.uk>
2019-04-26 11:41:24 +0100

    Keep default configs

--- src/Application.php
+++ src/Application.php
@@ -32,7 +32,7 @@ class Application
      */
     public function __construct(array $config)
     {
-        $this->config = $config;
+        $this->config = array_merge($this->config, $config);
 
         $this->connection = new Connection($this->config);
     }

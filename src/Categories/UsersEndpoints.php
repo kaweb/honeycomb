d5648b7e2acb9928efe47b074922b4a35e57ffc8
Joshua R. Sadler <joshua.sadler@kaweb.co.uk>
2019-09-19 15:36:29 +0100

    Added Revoke Api Key endpoint

--- src/Categories/UsersEndpoints.php
+++ src/Categories/UsersEndpoints.php
@@ -19,13 +19,17 @@ class UsersEndpoints extends BaseEndpoints
 	 * List Api Keys belonging to user
 	 * - Admin endpoint
 	 */
-	public function getApiKeys()
+	public function getApiKeys($client_id = null)
 	{
 		if (!$this->user_id) {
 			// Error: you must specify a user_id
 			return false;
 		}
 		$this->endpoint = '/api_keys';
+
+		if (!is_null($client_id)) {
+			$this->endpoint .= '/' . $client_id;
+		}
 
 		return $this->get($this->user_id);
 	}
@@ -42,5 +46,20 @@ class UsersEndpoints extends BaseEndpoints
 		}
 		$this->endpoint = '/' . $this->user_id . '/api_keys';
 		return $this->post($content);
+	}
+
+	/**
+	 * Revoke an API Key for a User
+	 * - Admin endpoint
+	 */
+	public function revokeApiKey($client_id)
+	{
+		if (!$this->user_id) {
+			// Error: you must specify a user_id
+			return false;
+		}
+		$this->endpoint = '/api_keys/' . $client_id;
+
+		return $this->get($this->user_id);
 	}
 }
